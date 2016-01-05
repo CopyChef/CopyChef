b0d19a92886800b8a19ca42efc75ee90328c6c18
Lidia Santos <Lidia@Lidias-MacBook-Pro.local>
2016-01-05 10:56:26 -0600

    adding backbone, some of it atleast

---
+++ public/javascript/app.js
@@ -0,0 +1,40 @@
+//namespacing
+var app = app || {};
+var active = active || {};
+
+app.Model = Backbone.Model.extend({
+});
+
+// define my 4 important parts!
+app.Collection = Backbone.Collection.extend({
+  model: app.Model, // what type of models will this collection hold?
+  url: '/recipes',
+  initialize: function() {
+    var self = this;
+    this.on('change', function() {
+      console.log('Our Collection changed.');
+      var view = new app.CollectionView({
+        collection: self
+      });
+    });
+    this.on('sync', function() {
+      console.log('Our Collection synced with the API.');
+      var view = new app.CollectionView({
+        collection: self
+      });
+    });
+    // get data from the API
+    this.fetch();
+  }
+});
+
+// mongoDB support!
+Backbone.Model.idAttribute = "_id";
+
+// the document is ready
+$(document).ready(function(){
+  // active.collection = new app.Collection();
+  // active.createRecipeView = new app.addRecipeView({
+  //   collection: active.collection
+  // }); this will be different for us once we get our layouts and such together
+});
