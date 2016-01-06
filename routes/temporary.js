153086971d7932afe3d15b2bb2dcddf5df08bc02
Stephen Delaney <swdelaney2@gmail.com>
2016-01-06 14:16:41 -0600

    deleted temp.js

--- routes/temporary.js
+++
@@ -1,9 +0,0 @@
-// Router for Restaurant search
-router.get('/search/:Restaurant', function(req, res, next) {
-  model.find({ Restaurant: req.params.Restaurant}, function (err, recipe){
-    if (err) console.log(error);
-    res.json(recipe);
-  });
-
-});
-// End router for Restaurant search
